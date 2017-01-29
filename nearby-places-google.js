3894170edcd240f806f117059a2d0b8d69dcdfe2
Sands <richardgsands@gmail.com>
2017-01-29 15:29:05 +0000

    testing package integration

--- nearby-places-google.js
+++ nearby-places-google.js
@@ -1,4 +1,18 @@
-// Write your package code here!
+// console.log('meteorich:nearby-places-google loaded');
+
+if (Meteor.isClient) {
+
+	Meteor.startup(function() {
+
+		console.log('meteorich:nearby-places-google - loading google');
+		
+
+	});
+
+}
+
+
+
 
 // Variables exported by this module can be imported by other packages and
 // applications. See nearby-places-google-tests.js for an example of importing.
